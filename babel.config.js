b6353f92ee8a965d219bfabb05a95336312c6373
zengzijian <zengzj@shinetech-china.com>
2019-11-21 16:12:26 +0800

    add： 添加babel装饰器

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,12 @@
+/*
+ * @Author: your name
+ * @Date: 2019-11-20 15:35:44
+ * @LastEditTime: 2019-11-21 14:15:54
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \xunwei_rn\babel.config.js
+ */
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [['@babel/plugin-proposal-decorators', {legacy: true}]],
 };
